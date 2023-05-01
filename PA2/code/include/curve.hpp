9d967a8701f5958bfc99edc4380a49a4732550a2
hws2002 <hanys21@mails.tsinghua.edu.cn>
2023-05-01 17:32:17 +0800

    add : Bezier Bernsteins

--- PA2/code/include/curve.hpp
+++ PA2/code/include/curve.hpp
@@ -9,6 +9,7 @@
 #include <vector>
 
 #include "object3d.hpp"
+// #define DEBUG
 
 // TODO (PA3): Implement Bernstein class to compute spline basis function.
 //       You may refer to the python-script for implementation.
@@ -63,18 +64,7 @@ class Curve : public Object3D {
         glEnd();
         glPopAttrib();
     }
-    void pad() {
-        int tSize = t.size();
-        tpad.resize(tSize + k);
-        for (int i = 0; i < tSize; ++i) tpad[i] = t[i];
-        for (int i = 0; i < k; ++i) tpad[i + tSize] = t.back();
-    }
-    int n, k;
-    std::vector<double> t;
-    std::vector<double> tpad;
-    int NCP, degree;
-    std::vector<double> Knots;
-    double range[2];
+    int NCP, degree; // Numbe of control points, and degree of curve
 };
 
 class BezierCurve : public Curve {
@@ -84,23 +74,77 @@ class BezierCurve : public Curve {
             printf("Number of control points of BezierCurve must be 3n+1!\n");
             exit(0);
         }
-        n = controls.size();
-        k = n - 1;
-        range[0] = 0;
-        range[1] = 1;
-        t.resize(2 * n);
-        for (int i = 0; i < n; ++i) {
-            t[i] = 0;
-            t[i + n] = 1;
-        }
-        pad();
+        NCP = points.size(); //number of control points = NCP
+        degree = NCP-1; // k = n = NCP-1
+        // Knots.resize(NCP+degree+1);
+        // for(int i=0;i <= degree;i++){
+            // Knots[i] = 0;
+        // }
+        // for(int i=NCP; i< Knots.size();i++){
+            // Knots[i] = 1;
+        // }
+
     }
     void discretize(int resolution, std::vector<CurvePoint>& data) override{
+        int num_data = resolution; //most of the situation, it would be resolution * (4-3)
+        data.resize(num_data);
+        for(int i=0; i<resolution;i++){
+            data[i].V = Vector3f::ZERO;
+            data[i].T = Vector3f::ZERO;
+            double step = double(i)/resolution;
+            
+            #ifdef DEBUG
+            std::cout<<i+1<<"th step is "<<step<<std::endl;
+            #endif
+
+            //Calculate the Bernsteins
+            std::vector<double> Bernsteins(NCP,0);
+            std::vector<double> DBernsteins(NCP,0);
+            for(int j=0; j<=NCP-1;j++){
+                Bernsteins[j] = get_Bernstein(j,step);
+                #ifdef DEBUG
+                std::cout<<"Bernsteins["<<j<<"] is "<<Bernsteins[j]<<std::endl;
+                #endif
+            } 
+            // Calculate the Derivatives
+            for(int j=0; j<=NCP-1;j++){
+                // DBernsteins[j] = ;
+            }
+            // ∑
+            for(int j=0; j<=NCP-1;j++){
+                data[i].V += controls[j] * Bernsteins[j];
+                data[i].T += controls[j] * DBernsteins[j];
+            }
+        }
+    }
+protected:
+    double get_Bernstein(int i, double t){
+        int n = NCP-1;
+        return C(n,i) * pow(t,i) * pow(1-t,n-i);
+    }
+
+    double C(int n,int i){
+        if(n==i || i==0)
+            return 1;
+        return factorial(n)/(factorial(i)*factorial(n-i));
+    }
+
+    double factorial(int n){
+        if(n<=1) return 1;
+
+        double res = 1;
+        while(n>1){
+            res *= n;
+            n--;
+        }
+        return res;
     }
 };
 
 class BsplineCurve : public Curve {
-   public:
+public:
+    std::vector<double> Knots;
+
     BsplineCurve(const std::vector<Vector3f> &points) : Curve(points) {
         if (points.size() < 4) {
             printf(
@@ -108,55 +152,56 @@ class BsplineCurve : public Curve {
                 "4!\n");
             exit(0);
         }
-                    NCP = points.size(); //number of control points
+        NCP = points.size(); //number of control points
         degree = 3; // pre-determined
         Knots.resize(NCP+degree+1);
         for(int i=0;i <NCP+degree+1;i++){
             Knots[i] = (double)i/(NCP+degree);
         }
-
-        n = controls.size();
-        k = 3;
-        t.resize(n + k + 1);
-        for (int i = 0; i < n + k + 1; ++i) t[i] = (double)i / (n + k);
-        pad();
-        range[0] = t[k];
-        range[1] = t[n];
     }
     void discretize(int resolution, std::vector<CurvePoint>& data) override{
         int num_data = resolution * (NCP - degree); //most of the situation, it would be resolution * (4-3)
 
         //debug
-        // std::cout<<"Number of points in this discretization is "<<num_data<<std::endl;
+        #ifdef DEBUG
+            std::cout<<"Number of points in this discretization is "<<num_data<<std::endl;
+        #endif
 
         data.resize(num_data);
 
         //debug
-        // for(int i=0; i<NCP+degree+1;i++){
-        //     std::cout<<"knots["<<i<<"] is "<<Knots[i]<<std::endl;
-        // }
+        #ifdef DEBUG
+        for(int i=0; i<NCP+degree+1;i++){
+            std::cout<<"knots["<<i<<"] is "<<Knots[i]<<std::endl;
+        }
+        #endif
 
         for (int i=degree;i<NCP;i++) {
             // get the index of Control points, that are effective at this iteration.
-            int Valid[4];
+            // int Valid[4];
+            int * Valid = new int[degree+1];
             
             for(int j = 0; j<=degree;j++){
                 Valid[j] = i-degree+j;
-                //debug
-                // cout<<"Valid["<<j<<"] is "<<Valid[j]<<endl;
+                #ifdef DEBUG
+                    cout<<"Valid["<<j<<"] is "<<Valid[j]<<endl;
+                #endif
             }
             
             //debug
-            // cout<<"Knots[i] is "<<Knots[i]<<endl;
-            // cout<<"Knots[i+1] is "<<Knots[i+1]<<endl;
-
+            #ifdef DEBUG
+                cout<<"Knots[i] is "<<Knots[i]<<endl;
+                cout<<"Knots[i+1] is "<<Knots[i+1]<<endl;
+            #endif
             for(int j=0;j<resolution;j++){
                 data[(i-degree)*resolution + j].V = Vector3f::ZERO;
                 // Calculate t
                 double step = (Knots[i+1] - Knots[i]) * ((double)j/(resolution)) + Knots[i];
 
                 // debug
-                std::cout<<"step is "<<step<<endl;
+                #ifdef DEBUG
+                    std::cout<<"step is "<<step<<endl;
+                #endif
                 // Dynamic programming, get Bernsteins
                 //  - for t in t_i to t_{i+1}, Bernsteins are only determined by the B_i,0
                 std::vector<double> Bernsteins(degree+1,0);
@@ -164,29 +209,33 @@ class BsplineCurve : public Curve {
                     Bernsteins[l] = get_Bernstein(Valid[l],degree,step);
 
                     //debug
-                    std::cout<<"Params are  "<<Valid[l]<<" "<<degree<<" "<<step<<endl;
-                    std::cout<<Bernsteins[l]<<endl;
+                    #ifdef DEBUG
+                        std::cout<<"Params are  "<<Valid[l]<<" "<<degree<<" "<<step<<endl;
+                        std::cout<<Bernsteins[l]<<endl;
+                    #endif
                 }
 
                 // ∑ 
-                std::cout<<"Point for this iteration is..(in data index)";
-                std::cout<<(i-degree)*resolution + j<<endl;
+
+                //debug
+                #ifdef DEBUG
+                    std::cout<<"Point for this iteration is..(in data index)";
+                    std::cout<<(i-degree)*resolution + j<<endl;
+                #endif
                 for (int l = 0; l < Bernsteins.size(); ++l) {
                     data[(i-degree)*resolution + j].V += controls[Valid[l]] * Bernsteins[l];
                     // data[i].T += controls[-lsk + j] * ds[j];
                 }
 
                 //debug
-                std::cout<<data[(i-degree)*resolution + j].V<<endl;
+                #ifdef DEBUG
+                    std::cout<<data[(i-degree)*resolution + j].V<<endl;
+                #endif
             }
+            delete [] Valid;
         }
-        // std::cout<<"Points are..."<<endl;
-        // for(int i=0;i<num_data;i++){
-        //     data[i].V = Vector3f(3+i,0,0);
-        //     std::cout<<data[i].V<<endl;
-        // }
-        // std::cout<<endl;
     }
+
 protected:
     double get_Bernstein(int i,int p,double t){
         if(p <= 0){
